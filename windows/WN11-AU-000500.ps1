bef824f07669c3f79748f8cc7823d73203de71f9
yareyab <156192077+yareyab@users.noreply.github.com>
2026-09-12 01:38:02 +0100

    Update WN11-AU-000500.ps1

--- windows/WN11-AU-000500.ps1
+++ windows/WN11-AU-000500.ps1
@@ -15,10 +15,10 @@
     Documentation   : https://stigaview.com/products/win11/v2r8/WN11-AU-000500/
 
 .TESTED ON
-    Date(s) Tested  : 2026-09-11
-    Tested By       : Eray Bay
-    Systems Tested  : Microsoft Windows 11 Pro 10.0.26200
-    PowerShell Ver. : 5.1.26100.9444
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
 
 .USAGE
     PS C:\> .\WN11-AU-000500.ps1
